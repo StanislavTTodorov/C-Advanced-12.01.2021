d9b3f62561e5dc14b4515f5239a2b4d331ec4ae2
StanislavTTodorov <77863355+StanislavTTodorov@users.noreply.github.com>
2023-07-08 22:37:25 +0300

    add Files

---
+++ 7-02.02.2021CustomDoublyLinkedList/7-02.02.2021CustomDoublyLinkedList/Overview/LinkedListBonus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    class LinkedListBonus
+    {
+        private bool reversed = false;
+
+        public Node Head { get; set; }
+
+        public Node Tail { get; set; }
+
+        public void Reverse()
+        {
+            Node temp = Head;
+            Head = Tail;
+            Tail = temp;
+            reversed = !reversed;
+        }
+        public void ForEach(Action<Node> action)
+        {
+            Node currentNode = Head; 
+            while (currentNode!= null)
+            {              
+                action(currentNode);
+                if (reversed)
+                {
+                    currentNode = currentNode.Previous; 
+
+                }
+                else
+                {
+                    currentNode = currentNode.Next;
+                }
+            }
+        }
+       
+        public void AddLast(Node node)
+        {
+            if (Tail == null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+            node.Previous = Tail;
+            Tail.Next = node;
+            Tail = node;
+        }
+    }
+}
---
+++ 7-02.02.2021CustomDoublyLinkedList/7-02.02.2021CustomDoublyLinkedList/Overview/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //LinkedListBonus list = new LinkedListBonus();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    list.AddLast(new Node(i + 1));
+            //}
+            //list.Reverse();
+            //list.Reverse();
+
+            //list.ForEach((node) =>
+            //{
+            //    Console.WriteLine(node.Value);
+            //});
+
+            //return;
+            LinkedList linkedList = new LinkedList();
+            for (int i = 0; i <= 5; i++)
+            {
+                linkedList.AddHead(new Node(i));
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                linkedList.AddLast(new Node(i));
+            }
+
+            Console.WriteLine("Remuve Tail " + linkedList.RemoveTail().Value);
+            Console.WriteLine("Remuve Head " + linkedList.RemoveHead().Value);
+            linkedList.ForEachHead((node) =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            int[] linkedListAsArray = linkedList.ToArray();
+        }
+    }
+}
