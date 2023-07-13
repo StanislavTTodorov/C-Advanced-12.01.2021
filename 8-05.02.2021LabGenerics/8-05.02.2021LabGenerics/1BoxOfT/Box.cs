f2a35e64600d00ca29e900404f81c2a4e38154d9
StanislavTTodorov <77863355+StanislavTTodorov@users.noreply.github.com>
2023-07-13 23:14:09 +0300

    add files

---
+++ 8-05.02.2021LabGenerics/8-05.02.2021LabGenerics/1BoxOfT/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+
+        public Box()
+        {
+           this.stack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.stack.Push(element);
+        }
+
+        public T Remove()
+        {
+           return this.stack.Pop();
+        }
+
+        public int Count()
+        {
+           return stack.Count();
+        }
+        
+    }
+}
