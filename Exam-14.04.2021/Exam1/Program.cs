d06b0c9fc469b9b9ba27ed910d3a4f69a94b0a36
StanislavTTodorov <77863355+StanislavTTodorov@users.noreply.github.com>
2023-10-03 18:45:45 +0300

    Add Files

---
+++ Exam-14.04.2021/Exam1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hatsStack = CreateStack();
+            Queue<int> scarfsQueue = CreateQueue();
+
+            List<int> set = new List<int>();
+
+            while (hatsStack.Count>0&&scarfsQueue.Count>0)
+            {
+                int hat = hatsStack.Peek();
+                int scar = scarfsQueue.Peek();
+                if (hat == scar)
+                {
+                    scarfsQueue.Dequeue();
+                    hatsStack.Pop();
+                    hatsStack.Push(hat + 1);
+                }
+                else if (hat < scar)
+                {
+                    hatsStack.Pop();
+                }
+                else if (hat > scar)
+                {
+                    int sum = hat + scar;
+                    set.Add(sum);
+                    scarfsQueue.Dequeue();
+                    hatsStack.Pop();
+                }
+            }
+            Console.WriteLine($"The most expensive set is: {set.Max()}");
+            Console.WriteLine(string.Join(" ",set));
+
+        }
+        private static Queue<int> CreateQueue()
+        {
+            Queue<int> newQueue = new Queue<int>();
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            foreach (var number in numbers)
+            {
+                newQueue.Enqueue(number);
+            }
+            return newQueue;
+        }
+
+        private static Stack<int> CreateStack()
+        {
+            Stack<int> newStack = new Stack<int>();
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            foreach (var number in numbers)
+            {
+                newStack.Push(number);
+            }
+            return newStack;
+        }
+    }
+}
---
+++ Exam-14.04.2021/Exam2/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> position = new List<int>() { 0, 0, 0 };
+
+            position[2] = int.Parse(Console.ReadLine());
+
+            string[][] matrix = Create(position);
+
+            bool successfullySavedPrincess = false;
+
+            while (position[2] >= 0&&!successfullySavedPrincess)
+            {
+                string[] commands = Console.ReadLine().Split(" ");
+                string command = commands[0];
+                matrix[int.Parse(commands[1])][ int.Parse(commands[2])] = "B";
+                switch (command.ToUpper())
+                {
+                    case "W":
+                        successfullySavedPrincess =  Move(position, matrix, -1, 0);
+                        break;
+                    case "S":
+                        successfullySavedPrincess = Move(position, matrix, 1, 0);
+                        break;
+                    case "A":
+                        successfullySavedPrincess = Move(position, matrix, 0, -1);
+                        break;
+                    case "D":
+                        successfullySavedPrincess = Move(position, matrix, 0, 1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //if (!successfullySavedPrincess&&position[2]<0)
+            //{
+            //    Console.WriteLine($"Mario died at {position[3]};{position[4]}.");
+            //}
+            if (successfullySavedPrincess)
+            {
+                Console.WriteLine($"Mario has successfully saved the princess! Lives left: {position[2]}");
+            }
+            else 
+            {
+                Console.WriteLine($"Mario died at {position[3]};{position[4]}.");
+
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool Move(List<int> position, string[][] matrix, int moveRow, int moveCol)
+        {
+            int row = position[0] + moveRow;
+            int col = position[1] + moveCol;
+            position[2] -= 1;
+            if (isIn(row,col,matrix))
+            {                
+                if (matrix[row][col]=="B")
+                {
+                    position[2] -= 2;
+                    if (position[2]>0)
+                    {
+                        matrix[row][ col] = "-";
+                    }
+                    else
+                    {
+                        matrix[row][ col] = "X";
+                        position.Add(row);
+                        position.Add(col);
+                    }                   
+                }
+
+                if (matrix[row][ col] == "P")
+                {
+                    matrix[row][ col] = "-";
+                    position[0] = row;
+                    position[1] = col;
+                    return true;
+                }
+               
+                position[0] = row;
+                position[1] = col;               
+            }
+            else
+            {
+                if (position[2]<=0&&isIn(row,col,matrix))
+                {
+                    matrix[row][ col] = "X";
+                    position.Add(row);
+                    position.Add(col);
+                }
+                else if(position[2] <= 0)
+                {
+                    matrix[position[0]][ position[1]] = "X";
+                    position.Add(position[0]);
+                    position.Add(position[1]);
+                }
+               
+            }
+            return false;
+
+        }
+
+        private static string[][] Create(List<int> position)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[][] matrix = new string[n][];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                string colInput = Console.ReadLine();
+                matrix[row] = new string[colInput.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = colInput[col].ToString();
+                    if (colInput[col] == 'M')
+                    {                      
+                        position[0] = row;
+                        position[1] = col;
+                        matrix[position[0]][position[1]] = "-";
+                    }
+                    //if (colInput[col] == 'B')
+                    //{
+                    //    position.Add(row);
+                    //    position.Add(col);
+                    //}
+                }
+            }
+            return matrix;
+        }
+
+        private static bool isIn(int row, int col, string[][] matrix)
+        {
+            bool on = row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix[row].Length;
+            return on;
+        }
+
+      
+    }
+}
